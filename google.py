68d40fab8bc4fc210304a952e158b8c4482d482e
Tivya1572 <71957463+Tivya1572@users.noreply.github.com>
2020-12-13 09:28:19 +0530

    Update google.py

--- google.py
+++ google.py
@@ -21,8 +21,8 @@ def get_google_api_connection():
     credential_path = os.path.join(CURRENT_DIR, CLIENT_SECRET_FILE)
     credentials = ServiceAccountCredentials.from_json_keyfile_name(credential_path, SCOPES)
     google_connection = gspread.authorize(credentials)
-    SHEET_ID = '1qTRyOfzg3gPht1FA4nvJCAMKMsyLnV90cAil0rzMFRE'
-    SHEET_NAME = 'Employee_details'
+    SHEET_ID = '____________________________________________________________'
+    SHEET_NAME = '______________________________________________-'
     googlesheetdata = google_connection.open_by_key(SHEET_ID).worksheet(SHEET_NAME).get_all_values()
     sheets_df = pd.DataFrame(googlesheetdata)
     sheetdata = sheets_df.drop(sheets_df.index[0])
@@ -32,8 +32,8 @@ def get_google_api_connection():
     for key, item in newval:
         group_df = newval.get_group(key)
         result = group_df.head(10)
-        spreadsheet_key = '1qTRyOfzg3gPht1FA4nvJCAMKMsyLnV90cAil0rzMFRE'
-        wks_name = 'Employees_top10_salary_deptwise'
+        spreadsheet_key = '_____________________________________________________'
+        wks_name = '__________________'
         d2g.upload(result, spreadsheet_key, wks_name, credentials=credentials, row_names=True)
 
 
